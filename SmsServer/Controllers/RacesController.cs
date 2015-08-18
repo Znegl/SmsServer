6d1fa791cfbc8c306ca6ed3c4ec299afc0ca99a3
David Fotel <davsebamse@gmail.com>
2015-08-18 21:07:42 +0200

    Så virker antal besvarelser korrekt

--- SmsServer/Controllers/RacesController.cs
+++ SmsServer/Controllers/RacesController.cs
@@ -44,7 +44,7 @@ namespace SmsServer.Controllers
             var username = GetUserNameFromRequest();
             ViewBag.TotalAnswers = (from a in db.Answers
                                    where a.Post.Race.Owner == username
-                                   group a by new { a.Post.RaceID, a.Post.Id } into g
+                                   group a by new { a.Post.RaceID } into g
                                    select new { g.Key, count = g.Count() }).ToDictionary(g => g.Key.RaceID, g => g.count);
             var user = GetUserNameFromRequest();
             return View(GetRaceForUser(null));
