b7d90aff4e6c22cfa98f92f7b5218cc0d4a81f93
Davse Bamse <davsebamse@gmail.com>
2016-02-25 22:42:49 +0100

    First part of answering posts on the web. Still need second part.

--- SmsServer/Controllers/HomeController.cs
+++ SmsServer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmsServer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace SmsServer.Controllers
 {
     public class HomeController : Controller
     {
+        private SmsServerContext context = new SmsServerContext();
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +28,69 @@ namespace SmsServer.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult AnswerPost(int postid)
+        {
+            var post = context.Posts.Find(postid);
+            if (post == null)
+            {
+                //404 not found
+                return HttpNotFound();
+            }
+            var race = post.Race;
+            Session["RaceId"] = race.Id;
+            if (Session["TeamId"] != null)
+            {
+                return RedirectToAction("ConfirmTeam", new { postid = postid });
+            }
+            else
+            {
+                return RedirectToAction("ChooseTeam", new { postid = postid });
+            }
+        }
+
+        public ActionResult ConfirmTeam(int postid)
+        {
+            var team = context.Teams.Find(Session["TeamId"]);
+            if (team == null)
+            {
+                return RedirectToAction("ChooseTeam");
+            }
+            ViewBag.PostId = postid;
+            return View("ConfirmTeam", team);
+        }
+
+        public ActionResult ChooseTeam(int postid)
+        {
+            var post = context.Posts.Find(postid);
+            if (post == null)
+            {
+                //404 not found
+                return HttpNotFound();
+            }
+            var race = post.Race;
+            Session["RaceId"] = race.Id;
+            Session["PostToAnswer"] = postid;
+            var teams = context.Teams.Where(t => t.Race.Id == race.Id).ToList();
+            ViewBag.PostId = postid;
+            return View("ChooseTeam", teams);
+        }
+
+        public ActionResult ChosenTeam(int teamid, int postid)
+        {
+            Session["TeamId"] = teamid;
+            return RedirectToAction("AnswerPost", "Home", new { postid = postid });
+        }
+
+        public ActionResult ShowPostForAnswer(int postid)
+        {
+            return RedirectToAction("ShowWebPost", "Posts", new { postid = postid });
+        }
+
+        public ActionResult CreateTeam(int postid)
+        {
+            return RedirectToAction("Create", "Teams", new { postid = postid });
         }
     }
 }
---
+++ SmsServer/Controllers/TeamsController.cs
@@ -0,0 +1,41 @@
+﻿using SmsServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmsServer.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private SmsServerContext context = new SmsServerContext();
+
+        // GET: Teams
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string TeamName)
+        {
+            if (string.IsNullOrEmpty(TeamName) || string.IsNullOrWhiteSpace(TeamName))
+                return View("Create");
+
+            var team = new Team();
+            team.TeamName = TeamName;
+            team.HoldID = TeamName;
+            var raceFromSession = (int)Session["RaceId"];
+            var race = context.Races.Where(r => r.Id == raceFromSession).FirstOrDefault();
+            if (race == null)
+                return RedirectToAction("Index", "Home");
+
+            team.Race = race;
+            context.Teams.Add(team);
+            context.SaveChanges();
+            Session["TeamId"] = team.Id;
+            return RedirectToAction("ShowPostForAnswer", "Home", new { postid=Session["PostToAnswer"] });
+        }
+    }
+}
